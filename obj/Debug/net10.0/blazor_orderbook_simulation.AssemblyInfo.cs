42c91539c63a7090713ddf33497de9b699423bbd
M0K0 <m0k097@gmx.de>
2026-05-12 16:10:52 +0200

    made a nice input_form component

--- obj/Debug/net10.0/blazor_orderbook_simulation.AssemblyInfo.cs
+++ obj/Debug/net10.0/blazor_orderbook_simulation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor_orderbook_simulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+116bf67d0a7b3eef3d01089eb2f9323b65a24598")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2edc75993e822388b1e07a74b665d0da0c4e5b1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor_orderbook_simulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor_orderbook_simulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
