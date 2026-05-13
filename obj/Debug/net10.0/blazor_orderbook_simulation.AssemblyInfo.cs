2e964df6e7b8a395d8eba32fcace87a68a1b99b8
M0K0 <m0k097@gmx.de>
2026-05-13 03:11:30 +0200

    safing working stable state

--- obj/Debug/net10.0/blazor_orderbook_simulation.AssemblyInfo.cs
+++ obj/Debug/net10.0/blazor_orderbook_simulation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor_orderbook_simulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1397ac6f91e6231546ade6e3aab5d848d7567510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176a68538987634674f351bdc586d9841ed00257")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor_orderbook_simulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor_orderbook_simulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
