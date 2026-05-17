10350f535babf4c97326f3382f3df10bbbebc156
M0K0 <m0k097@gmx.de>
2026-05-17 12:40:00 +0200

    redesigned execution table

--- obj/Debug/net10.0/blazor_orderbook_simulation.AssemblyInfo.cs
+++ obj/Debug/net10.0/blazor_orderbook_simulation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor_orderbook_simulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f88a91fb31dd9291ceadf0a9dbd95e07d4904af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c8994a6f2700e98fa3569acec8e2dbeb843582b")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor_orderbook_simulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor_orderbook_simulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
